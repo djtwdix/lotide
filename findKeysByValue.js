8fc7e17354468c1be52c0579391306cb8e24de66
Daniel Tweedie <djtwd.ix@icloud.com>
2021-02-03 20:54:58 +0000

    Created function that takes in an object and a value and returns the key that contains that value if one exists or undefined if it does not

---
+++ findKeysByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, val) {
+  const getKeys = Object.keys(obj);
+  for (const key of getKeys) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
