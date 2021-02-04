7f7d546bdda422a83a9d9f83024d91586e29ab74
Daniel Tweedie <djtwd.ix@icloud.com>
2021-02-04 01:14:57 +0000

    Commented out tests

--- eqObjects.js
+++ eqObjects.js
@@ -45,17 +45,21 @@ const eqObjects = function(obj1, obj2) {
   //else return true
   return true;
 };
+
 /*
+tests
+
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 //console.log(eqObjects(ab, ba));
 
 const abc = { a: "1", b: "2", c: "3" }
 //console.log(eqObjects(ab, abc));
-*/
+
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 console.log(eqObjects(cd, dc)); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 console.log(eqObjects(cd, cd2)); // => false
+*/
