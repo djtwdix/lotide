40ce14c663c78f6b1e2de96983d8c001407527a7
Daniel Tweedie <djtwd.ix@icloud.com>
2021-02-06 14:26:32 +0000

    Implemented feature that checks for nested arrays and compares them for equality using recursion

--- eqArrays.js
+++ eqArrays.js
@@ -3,9 +3,21 @@ const eqArrays = function(arr1, arr2) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+    if (Array.isArray(arr1[i])) {
+      if (eqArrays(arr1[i], arr2[i]) === false) {
+        return false;
+      }
+    }
+    if (!Array.isArray && arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
 };
+
+//recursion test cases
+/*
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // => true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])) // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
+*/
