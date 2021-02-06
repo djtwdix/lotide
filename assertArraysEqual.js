594b1f69f2c592d9d764ed9fd322b104795afcd7
Daniel Tweedie <djtwd.ix@icloud.com>
2021-02-06 14:27:34 +0000

    Updated eqArrays function to include recursion case

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,7 +3,12 @@ const eqArrays = function(arr1, arr2) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+    if (Array.isArray(arr1[i])) {
+      if (eqArrays(arr1[i], arr2[i]) === false) {
+        return false;
+      }
+    }
+    if (!Array.isArray && arr1[i] !== arr2[i]) {
       return false;
     }
   }
