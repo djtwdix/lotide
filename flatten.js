437d68f2cde6be4ef0a09e81db57a7b02bcdacf3
djtwdix <djtwd.ix@icloud.com>
2021-02-07 14:04:59 -0400

    Imported assertEqual for testing, added module.exports value, added descriptive comments

--- flatten.js
+++ flatten.js
@@ -1,13 +1,25 @@
+//imports
+const assertEqual = require("./assertEqual")
+
+//function that takes in nested arrays and returns single array
 const flatten = function(arr) {
+  //initialize empty array
   let flattenedArray = [];
+  //iterate over input array
   for (let i = 0; i < arr.length; i++) {
+    //if array[i] is array
     if (Array.isArray(arr[i])) {
+      //iterate over array[i] and push all elements within to flattenedArray
       for (let j = 0; j < arr[i].length; j++) {
         flattenedArray.push(arr[i][j]);
       }
+    //if array[i] is not an array, push arr[i] to flattenedArray
     } else {
       flattenedArray.push(arr[i]);
     }
   }
+  //return flattenedArray
   return flattenedArray;
 }
+
+module.exports = flatten;
