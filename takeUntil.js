bf1e44015da958daff337ab46dd6f2599bd57788
djtwdix <djtwd.ix@icloud.com>
2021-02-07 13:30:09 -0400

    Added export funcionality, imported assertArraysEqual for testing and added descriptive comments

--- takeUntil.js
+++ takeUntil.js
@@ -1,42 +1,16 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+//imports
+const assertArraysEqual = require("./assertArraysEqual")
 
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual,expected) === true) {
-    console.log(`✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
+//Function that takes in array and a callback function
 const takeUntil = (array, callback) => {
+  //iterate over array
   for (let i = 0; i < array.length; i++) {
     const item = array[i];
+    //callback function returns true with array[i] slice and return the array from 0 to index i
     if (callback(item) === true) {
       return array.slice(0, i);
     }
   }
 };
 
-//test cases
-/*
-const data1 = [1,-3,4];
-
-const results1 = takeUntil(data1, x => x < 0);
-
-const data2 = ["Daniel", ",", "Alex"];
-const results2 = takeUntil(data2, x => x === ',');
-
-
-assertArraysEqual(results1, [1]);
-assertArraysEqual(results2, ["Daniel"]);
-*/
+module.exports = takeUntil;
