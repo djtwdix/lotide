9b6b599a9c35fd4df8e2c75b67c16a8b04847f65
djtwdix <djtwd.ix@icloud.com>
2021-02-07 13:18:11 -0400

    Added export functionality, imported eqArrays and added descriptive comments

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (Array.isArray(arr1[i])) {
-      if (eqArrays(arr1[i], arr2[i]) === false) {
-        return false;
-      }
-    }
-    if (!Array.isArray && arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays")
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual,expected) === true) {
