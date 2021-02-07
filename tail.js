c25edf788eff6538d7c322e2ad636a16be608070
djtwdix <djtwd.ix@icloud.com>
2021-02-07 14:27:26 -0400

    Imported assertEqual for testing, added module.exports value and descriptive comments

--- tail.js
+++ tail.js
@@ -1,6 +1,17 @@
+//import
+const assertEqual = require("./assertEqual");
+
+//Function that takes in array and returns all but the first element
 const tail = function(arr) {
+  //intialize empty tailArr array
   let tailArr = [];
+  //iterate over input array starting at position 1
   for (let i = 1; i < arr.length; i++) {
+    //push all array items to tailArr
     tailArr.push(arr[i]);
-  } return tailArr;
+  } 
+  //return tailArr
+  return tailArr;
 };
+
+module.exports = tail;
