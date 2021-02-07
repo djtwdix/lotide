d7f4bc5a2c7d71040dcece29ae1e9037723ca9bc
djtwdix <djtwd.ix@icloud.com>
2021-02-07 13:21:51 -0400

    Imported eqObjects, added export functionality and added descriptive comments

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,59 +1,16 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+//imports
+const eqObjects = require("./eqObjects")
 
-const eqObjects = function(obj1, obj2) {
-  const objOneKeys = Object.keys(obj1);
-  const objTwoKeys = Object.keys(obj2);
-  //if object key lengths don't match return false
-  if (objOneKeys.length !== objTwoKeys.length) {
-    return false;
-  }
-  //iterate through object one keys
-  for (const key of objOneKeys) {
-    //if key value is an array and arrays are not equal, return false
-    if (Array.isArray(obj1[key])) {
-      if (!eqArrays(obj1[key], obj2[key])) {
-        return false;
-      }
-    }
-    //if key value is object and not array, if objects are not equal, return false
-    if (typeof obj1[key] === "object" && !Array.isArray(obj1[key])) {
-      if (eqObjects(obj1[key], obj2[key]) === false) {
-        return false;
-      }
-    }
-    //if key value is not an array and values don't match return false
-    if (!Array.isArray(obj1[key]) && typeof obj1[key] !== "object") {
-      if (obj1[key] !== obj2[key]) {
-        return false;
-      }
-    }
-  }
-  //else return true
-  return true;
-};
-
+//function that takes in two objects, actual and expected
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
+  //if actual and expected are equal print assertion passed statement
   if (eqObjects(actual,expected) === true) {
     console.log(`✅ Assertion passed: ${inspect(actual)} === ${inspect(expected)}`);
+  //if actual and expected are not equal print assertion failed statement
   } else {
     console.log(`❌ Assertion failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
-/*
-Test
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
 
-assertObjectsEqual(cd, dc);
-*/
+module.exports = assertObjectsEqual
