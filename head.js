c13645e938dbd2b1ae7f857a43147d6773fd1a1f
djtwdix <djtwd.ix@icloud.com>
2021-02-07 14:06:31 -0400

    imported assertEqual for testing, added module.exports value and descriptive comments

--- head.js
+++ head.js
@@ -1,4 +1,9 @@
+//imports
+const assertEqual = require("./assertEqual");
+
+//Function that takes in array and returns it's first element
 const head = function(arr) {
   return arr[0];
 }
 
+module.exports = head;
