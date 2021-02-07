bdc483ef400a1cc5fee8f4d2a3012af9fecc168e
djtwdix <djtwd.ix@icloud.com>
2021-02-07 13:13:44 -0400

    Added export functionality and descriptive comments

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,12 @@
+//Function that takes in two parameters, actual and expected
 const assertEqual = function(actual, expected) {
+  //if actual equals expected print assertion passed statement
   if (actual === expected) {
     console.log(`✅ Assertion passed: ${actual} === ${expected}`);
+  // if they do not equal print assertion failed statement
   } else {
     console.log(`❌ Assertion failed: ${actual} !== ${expected}`);
   }
 };
+
+module.exports = assertEqual;
