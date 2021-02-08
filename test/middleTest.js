70e28a28a32a5fc26b87449929967bd645afb558
djtwdix <djtwd.ix@icloud.com>
2021-02-08 15:17:00 -0400

    Converted test to mocha syntax

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,17 @@
-const assertArraysEqual = require("../assertArraysEqual");
+//const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 const middle = require("../middle");
-
+/*
 assertArraysEqual(middle([1,2,3,4,5]), [3]);
 assertArraysEqual(middle([1,2,3,4]), [2,3]);
 assertArraysEqual(middle([1,2]), []);
+*/
 
+describe("#middle", () => {
+  it ("should return [3] when passed in [1,2,3,4,5]", () => {
+    assert.deepEqual(middle([1,2,3,4,5]), [3]);
+  });
+  it ("should return [3, 4] when passed in [1,2,3,4,5,6]", () => {
+    assert.deepEqual(middle([1,2,3,4,5,6]), [3,4]);
+  });
+});
