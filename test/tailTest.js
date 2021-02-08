1fea285b01f1d8f4895d15cc6380dc515ce9398a
djtwdix <djtwd.ix@icloud.com>
2021-02-08 15:16:31 -0400

    Converted test to mocha and chai syntax

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,14 @@
 const tail = require("../tail");
+const assert = require("chai").assert;
 const assertArraysEqual = require("../assertArraysEqual")
 
-assertArraysEqual(tail(["h","e","l","l","o"]), ["e","l","l","o"]);
+//assertArraysEqual(tail(["h","e","l","l","o"]), ["e","l","l","o"]);
+
+describe("#tail", () => {
+  it ("should return [2,3] when passed in [1,2,3]", () => {
+    assert.deepEqual(tail([1,2,3]), [2,3]);
+  });
+  it ("should return ['e','l','l','o'] when passed in ['h','e','l','l','o']", () => {
+    assert.deepEqual(tail(["h","e","l","l","o"]), ["e","l","l","o"]);
+  });
+});
